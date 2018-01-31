3d14d198cc058b4289f0e3e873e53e97f759e53a
Wenxi Zhou <vic.zwx1990@gmail.com>
2018-01-30 22:51:16 -0800

    beautified html app.js fix error alert

--- js/app.js
+++ js/app.js
@@ -5,12 +5,12 @@ var infoWindow = {};
 // Foursquare API Client
 var foursquareUrl = "https://api.foursquare.com/v2/venues/";
 var clientID = '&client_id=PPJCMNGT53VGKCOI3L5XF4LQUZKEAMHBOC3VXQOT5IPT3V3R';
-    clientSecret = '&client_secret=0ZF4N3S3MYQIKTIWT1TW3OZVPYKJTWJDGXXT3R3FK5N1BVK0';
+clientSecret = '&client_secret=0ZF4N3S3MYQIKTIWT1TW3OZVPYKJTWJDGXXT3R3FK5N1BVK0';
 
 // The orignal js is from: https://bootsnipp.com/snippets/featured/admin-side-menu
 // Use this snippets to complement function on side menu
-$(function () {
-    $('.navbar-toggle').click(function () {
+$(function() {
+    $('.navbar-toggle').click(function() {
         $('.navbar-nav').toggleClass('slide-in');
         $('.side-body').toggleClass('body-slide-in');
         $('#search').removeClass('in').addClass('collapse').slideUp(200);
@@ -18,8 +18,8 @@ $(function () {
         /// uncomment code for absolute positioning tweek see top comment in css
         //$('.absolute-wrapper').toggleClass('slide-in');
     });
-   // Remove menu for searching
-   $('#search-trigger').click(function () {
+    // Remove menu for searching
+    $('#search-trigger').click(function() {
         $('.navbar-nav').removeClass('slide-in');
         $('.side-body').removeClass('body-slide-in');
 
@@ -32,11 +32,13 @@ $(function () {
 // Create a map object and specify the DOM element for display
 function initMap() {
     // Creates a new map
-    map = new google.maps.Map(document.getElementById('map'),
-    {
-    center: {lat: 38.8878878, lng: -77.0565289},
-    zoom: 13,
-    // styles: styles
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: 38.8878878,
+            lng: -77.0565289
+        },
+        zoom: 13,
+        // styles: styles
     });
 
     // Apply bindings to ViewModel
@@ -54,13 +56,12 @@ var Place = function(data) {
 
 var ViewModel = function() {
     var self = this;
-
     self.placesList = ko.observableArray([]);
     self.searchInput = ko.observable('');
 
-    neighborObjArray.forEach(function(data) {  
-        var api_url = foursquareUrl + data.id + '/tips?' + clientID + clientSecret + '&v=20180128';
-
+    neighborObjArray.forEach(function(data) {
+        var api_url = `${foursquareUrl + data.id}/tips?${clientID + clientSecret}&v=20180128`;
+        console.log(api_url);
         var place = new Place(data);
         // Marker information
         var marker = new google.maps.Marker({
@@ -77,24 +78,26 @@ var ViewModel = function() {
         // This function add animation on markers when click on markers
         function toggleBounce() {
             if (marker.getAnimation() !== null) {
-              marker.setAnimation(null);
-            } else {
-              marker.setAnimation(google.maps.Animation.BOUNCE);
-              setTimeout(function(){
                 marker.setAnimation(null);
-            }, 2000);
+            } else {
+                marker.setAnimation(google.maps.Animation.BOUNCE);
+                setTimeout(function() {
+                    marker.setAnimation(null);
+                }, 2000);
             }
         }
         // Request data with AJAX
         var data = $.ajax({
-            url: api_url,
-            data: {format: 'json'},
-            dataType: 'json',
-            timeOut: 20
-        })
-        .fail(function() {
-            alert("Oops! Failed to load api");
-        });   
+                url: api_url,
+                data: {
+                    format: 'json'
+                },
+                dataType: 'json',
+                timeOut: 20
+            })
+            .fail(function() {
+                alert("Oops! Failed to load api");
+            });
 
         // Marker and infowindow event listener
         marker.addListener('click', function() {
@@ -103,19 +106,17 @@ var ViewModel = function() {
         });
     });
 
-
     self.filteredList = ko.computed(function() {
-        return ko.utils.arrayFilter(self.placesList(), function(place){
-            if(place.title().toLowerCase().indexOf(self.searchInput().toLowerCase()) >= 0) {
+        return ko.utils.arrayFilter(self.placesList(), function(place) {
+            if (place.title().toLowerCase().indexOf(self.searchInput().toLowerCase()) >= 0) {
                 place.marker.setVisible(true);
                 return true;
-            }else{
+            } else {
                 place.marker.setVisible(false);
                 return false;
             }
         })
     })
-
     self.placeClicked = function(place) {
         google.maps.event.trigger(place.marker, 'click');
     };
@@ -123,6 +124,7 @@ var ViewModel = function() {
     // Create infoWindow to show details on each restaurant
     var largeInfowindow = new google.maps.InfoWindow();
     var bounds = new google.maps.LatLngBounds();
+
     function populateInfoWindow(marker, infowindow, data) {
         // Pull tips text from Foursquare API
         var text = data.responseJSON.response.tips.items[0].text;
@@ -130,22 +132,23 @@ var ViewModel = function() {
         if (infowindow.marker != marker) {
             // infowindow.setContent('<div>' + marker.title + '</div>');
             infowindow.marker = marker;
-        // Make sure the marker property is cleared if the window is closed.
+            // Make sure the marker property is cleared if the window is closed.
             infowindow.addListener('closeclick', function() {
                 infowindow.setMarker = null;
             });
-        // Add street view service in infowindow
-        var streetViewService = new google.maps.StreetViewService();
-        // Set radius to 50 meters in case there is no street view of our specific latLng
-        var radius = 50;
-        // If the case of status is OK, which means the pano was found, compute the position of the streetview image, then
-        // get a panorama from that and set the options
-        function getStreetView(data, status) {
-            if (status == google.maps.StreetViewStatus.OK) {
-                var nearStreetViewLocation = data.location.latLng;
-                var heading = google.maps.geometry.spherical.computeHeading(
-                    nearStreetViewLocation, marker.position);
-                    infowindow.setContent('<div style="font-size: 18px; color: #051487;">' + marker.title + '</div><div id="pano"></div>' + '<div id="tips">' + text + '</div>');
+            // Add street view service in infowindow
+            var streetViewService = new google.maps.StreetViewService();
+            // Set radius to 50 meters in case there is no street view of our specific latLng
+            var radius = 50;
+            // If the case of status is OK, which means the pano was found, compute the position of the streetview image, then
+            // get a panorama from that and set the options
+            function getStreetView(data, status) {
+                if (status == google.maps.StreetViewStatus.OK) {
+                    var nearStreetViewLocation = data.location.latLng;
+                    var heading = google.maps.geometry.spherical.computeHeading(
+                        nearStreetViewLocation, marker.position);
+                    infowindow.setContent(`<div style="font-size: 18px; color: #051487;"> ${marker.title} </div><div id="pano"></div>
+                        <div id="tips"> ${text}</div>`);
                     var panoramaOptions = {
                         position: nearStreetViewLocation,
                         pov: {
@@ -153,30 +156,21 @@ var ViewModel = function() {
                             pitch: 30
                         }
                     };
-                var panorama = new google.maps.StreetViewPanorama(
-                    document.getElementById('pano'), panoramaOptions);
-            } else {
-                infowindow.setContent('<div>' + marker.title + '</div>' +
-                   '<div>No Street View Found</div>');
+                    var panorama = new google.maps.StreetViewPanorama(
+                        document.getElementById('pano'), panoramaOptions);
+                } else {
+                    infowindow.setContent(`<div>${marker.title}</div>
+                   <div>No Street View Found</div>`);
+                }
             }
-        }
-        // Use street view service to get cloest street view images within 50 meters of marker's position
-        streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
-        infowindow.open(map, marker);
+            // Use street view service to get cloest street view images within 50 meters of marker's position
+            streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
+            infowindow.open(map, marker);
         }
     }
 }
 
 // Fail to load google map
 function googleError() {
-     alert("Oops! Failed to load google map!");
+    alert("Oops! Failed to load google map!");
 }
-
-
-
-
-
-
-
-
-
